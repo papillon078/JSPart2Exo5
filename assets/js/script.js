922fa69c33e3603780f137e5b1057e3bcc66a572
papillon078 <ldc.morin@gmail.com>
2019-10-22 22:17:19 +0200

    exo6 avant correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ let path = 'assets/img/'+imgNbr+'_2.jpg';
 let oldImage = document.getElementById(imgNbr);
 oldImage.setAttribute("src", path);
 }
+function reloadImg(imgNbr){
+  let path = 'assets/img/'+imgNbr+'.jpg';
+  let oldImage = document.getElementById(imgNbr);
+  oldImage.setAttribute("src", path);
+}
